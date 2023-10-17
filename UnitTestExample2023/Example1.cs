a6107d19e9bd3aa9e13cd73195dd1f0f120f7d5b
YuAn <anm821@gmail.com>
2023-10-17 17:36:35 +0800

    Add Example1 Addition

---
+++ UnitTestExample2023/Example1.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitTestExample2023;
+
+public class Example1
+{
+    public int Addition(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ UnitTestExample2023/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace UnitTestExample2023;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
