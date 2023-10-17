e865b69578ac02eccfd4b68d9cf61bcd7f746503
YuAn <anm821@gmail.com>
2023-10-17 17:39:47 +0800

    Example2 Complete

---
+++ UnitTestExample2023/Example2.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime;
+
+namespace UnitTestExample2023;
+
+public class Example2
+{
+    public bool TimeUp()
+    {
+        if (Profile.SettingTime >= DateTime.Now)
+        {
+            Console.WriteLine("Time is up.");
+            return true;
+        }
+        
+        Console.WriteLine("Time is almost up.");
+        return false;
+    }
+}
+
+public static class Profile
+{
+    public static DateTime SettingTime { get; set; }
+}
