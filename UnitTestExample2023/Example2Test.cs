89d5146438da4af8c3826e5e5984599815783e37
YuAn <anm821@gmail.com>
2023-10-18 16:47:27 +0800

    Add Setup and move new fake example to setup

--- UnitTestExample2023/Example2Test.cs
+++ UnitTestExample2023/Example2Test.cs
@@ -1,60 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace UnitTestExample2023;
 
-namespace UnitTestExample2023
+public class Example2Test
 {
-    public class Example2Test
+    private readonly DateTime _now = new(2023, 12, 25, 0, 0, 0);
+    private readonly DateTime _settingTime = new(2023, 12, 24, 23, 59, 59);
+    private FakeExample2 _example2;
+
+    [SetUp]
+    public void SetUp()
     {
-        private readonly DateTime _settingTime = new DateTime(2023, 12, 24, 23, 59, 59);
-        private readonly DateTime _now = new DateTime(2023, 12, 25, 0, 0, 0);
-
-        [Test]
-        public void TimeUpTest_time_is_almost_up()
-        {
-            var example2 = new FakeExample2();
-            example2.SetSettingTime(_settingTime);
-            example2.SetNow(_now);
-
-            var actual = example2.TimeUp();
-            Assert.AreEqual(false, actual);
-            Assert.AreEqual("Time is almost up.", example2.ActualPrintStr);
-        }
+        _example2 = new FakeExample2();
     }
 
-    public class FakeExample2 : Example2
+    [Test]
+    public void TimeUpTest_time_is_almost_up()
     {
-        private DateTime _settingTime;
-        private DateTime _now;
-        private string _actualPrintStr;
+        _example2.SetSettingTime(_settingTime);
+        _example2.SetNow(_now);
 
-        public string ActualPrintStr => _actualPrintStr;
+        var actual = _example2.TimeUp();
+        Assert.AreEqual(false, actual);
+        Assert.AreEqual("Time is almost up.", _example2.ActualPrintStr);
+    }
+}
 
-        public void SetSettingTime(DateTime settingTime)
-        {
-            _settingTime = settingTime;
-        }
+public class FakeExample2 : Example2
+{
+    private DateTime _now;
+    private DateTime _settingTime;
 
-        public void SetNow(DateTime now)
-        {
-            _now = now;
-        }
+    public string ActualPrintStr { get; private set; }
 
-        protected override void ConsoleWriteLine(string printStr)
-        {
-            _actualPrintStr = printStr;
-        }
+    public void SetSettingTime(DateTime settingTime)
+    {
+        _settingTime = settingTime;
+    }
 
-        protected override DateTime GetNow()
-        {
-            return _now;
-        }
+    public void SetNow(DateTime now)
+    {
+        _now = now;
+    }
 
-        protected override DateTime GetSettingTime()
-        {
-            return _settingTime;
-        }
+    protected override void ConsoleWriteLine(string printStr)
+    {
+        ActualPrintStr = printStr;
+    }
+
+    protected override DateTime GetNow()
+    {
+        return _now;
+    }
+
+    protected override DateTime GetSettingTime()
+    {
+        return _settingTime;
     }
 }
