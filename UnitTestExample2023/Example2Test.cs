a14b84525f945f65b5ff7cdfdb7d55314ab15c51
YuAn <anm821@gmail.com>
2023-10-18 16:29:56 +0800

    Example2 Add Test of TimeUpTest_time_is_almost_up

---
+++ UnitTestExample2023/Example2Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample2023
+{
+    public class Example2Test
+    {
+        [Test]
+        public void TimeUpTest_time_is_almost_up()
+        {
+            var example2 = new Example2();
+
+            var actual = example2.TimeUp();
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
