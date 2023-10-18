3bf871438f478253e709f7aab2d298be319bf79b
YuAn <anm821@gmail.com>
2023-10-18 16:17:41 +0800

    AdditionTest_1_add_2_then_3 Test Success

--- UnitTestExample2023/Example1Test.cs
+++ UnitTestExample2023/Example1Test.cs
@@ -2,5 +2,11 @@
 
 public class Example1Test
 {
-    
+    [Test]
+    public void AdditionTest_1_add_2_then_3()
+    {
+        var example1 = new Example1();
+        var actual = example1.Addition(1, 2);
+        Assert.AreEqual(3,actual);
+    }
 }
