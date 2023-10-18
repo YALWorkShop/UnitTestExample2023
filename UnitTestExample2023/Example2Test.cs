9801d99de075e595d3260faa6a2cf34ddc2913ff
YuAn <anm821@gmail.com>
2023-10-18 16:43:47 +0800

    Give time to GetNow

--- UnitTestExample2023/Example2Test.cs
+++ UnitTestExample2023/Example2Test.cs
@@ -9,12 +9,14 @@ namespace UnitTestExample2023
     public class Example2Test
     {
         private readonly DateTime _settingTime = new DateTime(2023, 12, 24, 23, 59, 59);
+        private readonly DateTime _now = new DateTime(2023, 12, 25, 0, 0, 0);
 
         [Test]
         public void TimeUpTest_time_is_almost_up()
         {
             var example2 = new FakeExample2();
             example2.SetSettingTime(_settingTime);
+            example2.SetNow(_now);
             var actual = example2.TimeUp();
             Assert.AreEqual(false, actual);
         }
@@ -23,10 +25,16 @@ namespace UnitTestExample2023
     public class FakeExample2 : Example2
     {
         private DateTime _settingTime;
+        private DateTime _now;
 
         public void SetSettingTime(DateTime settingTime)
         {
             _settingTime = settingTime;
+        }
+
+        public void SetNow(DateTime now)
+        {
+            _now = now;
         }
 
         protected override void ConsoleWriteLine(string printStr)
@@ -36,7 +44,7 @@ namespace UnitTestExample2023
 
         protected override DateTime GetNow()
         {
-            return base.GetNow();
+            return _now;
         }
 
         protected override DateTime GetSettingTime()
