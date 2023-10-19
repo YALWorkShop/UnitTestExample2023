5dd6e84cd3b6bcf1bf59f32b7ecc5d19ffc873ce
YuAn <anm821@gmail.com>
2023-10-19 11:14:02 +0800

    Initialize field in setup

--- UnitTestExample2023/Example3Test.cs
+++ UnitTestExample2023/Example3Test.cs
@@ -1,36 +1,38 @@
-using NSubstitute;
-using System;
 using ExpectedObjects;
+using NSubstitute;
 using NSubstitute.ExceptionExtensions;
 
 namespace UnitTestExample2023;
 
 public class Tests
 {
-    // Add nuget Package FluentAssertions , NSubstitute , ExpectedObject
+    private Example3 _example3;
+    private IMemberDao _memberDao;
+    private IUtil _util;
+
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
+        _memberDao = Substitute.For<IMemberDao>();
+        _util = Substitute.For<IUtil>();
+        _example3 = new Example3(_memberDao, _util);
     }
+
+    // Add nuget Package FluentAssertions , NSubstitute , ExpectedObject
 
     [Test]
     public async Task SetMember_insert_member_exception_occur()
     {
-        IUtil util = Substitute.For<IUtil>();
-        IMemberDao memberDao = Substitute.For<IMemberDao>();
-        var example3 = new Example3(memberDao, util);
-
         var inputMember = GenerateMember("IT012", "EMIAL@email.com", "0978123456", new DateTime());
         var expectedMember = GenerateMember("IT012", "EMIAL@email.com", "0978123456", new DateTime());
         var exception = new Exception("Test Error");
 
-        memberDao.QueryMember(Arg.Is(inputMember.Id)).Returns(Task.FromResult<Member?>(null));
-        memberDao.InsertMember(Arg.Is<Member>(p => expectedMember.ToExpectedObject().Matches(p))).Throws(exception);
+        _memberDao.QueryMember(Arg.Is(inputMember.Id)).Returns(Task.FromResult<Member?>(null));
+        _memberDao.InsertMember(Arg.Is<Member>(p => expectedMember.ToExpectedObject().Matches(p))).Throws(exception);
 
-        var actual = await example3.SetMember(inputMember);
+        var actual = await _example3.SetMember(inputMember);
         Assert.AreEqual($"Error 58825252 : {exception.Message}", actual);
-
-        util.Received(1).SetExceptionLog(Arg.Is(exception.Message), Arg.Is(nameof(Example3.SetMember)));
+        _util.Received(1).SetExceptionLog(Arg.Is(exception.Message), Arg.Is(nameof(Example3.SetMember)));
     }
 
     private static Member GenerateMember(string id, string email, string phone, DateTime updateTime)
