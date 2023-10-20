37a451c77125bc78d88dfaa8558c82991e1b329b
VickyLi <v838111848@gmail.com>
2023-10-20 15:51:03 +0800

    add FakeBlogService for overriding GetBlogId, add SetBlogId for test arrangement. Revise Assert to check Id property

--- Homework/BlogServiceTest.cs
+++ Homework/BlogServiceTest.cs
@@ -5,21 +5,49 @@ namespace Homework
 {
     public class BlogServiceTest
     {
+        FakeBlogService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new FakeBlogService();
+        }
+
         [Test]
         public void CreateBlog_create_blog_Success_with_Id()
         {
-            var service = new BlogService();
-
             var createModel = new BlogCreateModel()
             {
                 Name = "It's me",
                 Introduction = "write something here..."
             };
 
-            var act = service.CreateBlog(createModel);
+            GivenBlogId("blog1");
 
-            Assert.IsTrue(act.IsSuccess);
-            Assert.IsNotNull(act.Result.Id);
+            var act = _service.CreateBlog(createModel);
+
+            Assert.That(act.IsSuccess, Is.True);
+            Assert.That(act.Result.Id, Is.EqualTo("blog1"));
+        }
+
+        private void GivenBlogId(string blogId)
+        {
+            _service.SetBlogId(blogId);
+        }
+    }
+
+    public class FakeBlogService : BlogService
+    {
+        private string _blogId;
+
+        public void SetBlogId(string blogId)
+        {
+            _blogId = blogId;
+        }
+
+        protected override string GetBlogId()
+        {
+            return _blogId;
         }
     }
 }
