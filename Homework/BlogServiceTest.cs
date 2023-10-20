53af56b5357b902ddcdb830a03cac31e321aac83
VickyLi <v838111848@gmail.com>
2023-10-20 12:48:49 +0800

    add test class "BlogServiceTest" and test case"CreateBlog_create_blog_Success_with_Id"

---
+++ Homework/BlogServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Homework
+{
+    public class BlogServiceTest
+    {
+        [Test]
+        public void CreateBlog_create_blog_Success_with_Id()
+        {
+
+        }
+    }
+}
